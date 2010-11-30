80aa6f75c6401c13c08a4c5858b30d0d74a65ddd
Livingston Samuel <livingston.samuel@gmail.com>
2010-11-30 16:08:17 +0530

    Added Grid

--- tynnu.js
+++ tynnu.js
@@ -5,3 +5,49 @@
      |_______|  |__|     \____/     |__|
         HAND CODED BY LIVINGSTON SAMUEL
 */
+/*! tynnu.js
+
+  @author - Livingston Samuel
+  @version - 0.1
+*/
+
+(function (window, document) {
+  var body = document.body,
+      gridH = body.clientHeight,
+      gridW = body.clientWidth,
+      gew = 10,
+      canvas = document.createElement('canvas'),
+      context = canvas.getContext('2d'),
+      n_x = parseInt(gridH/gew, 10) + 1,
+      n_y = parseInt(gridW/gew, 10) + 1,
+      t;
+
+  canvas.width = gridW;
+  canvas.height = gridH;
+  canvas.style.position = 'absolute';
+  canvas.style.top = '0';
+  canvas.style.left = '0';
+  canvas.style.zIndex = '1000000';
+  context.lineWidth = 0.3;
+  context.strokeStyle = 'rgba(0, 0, 0, 0.5)';
+
+  context.beginPath(0, 0);
+
+  while (n_x--) {
+    t = n_x * gew;
+    context.moveTo(0, t);
+    context.lineTo(gridW, t);
+  }
+
+  while (n_y--) {
+    t = n_y * gew;
+    context.moveTo(t, 0);
+    context.lineTo(t, gridH);
+  }
+
+  context.stroke();
+  context.fill();
+  context.save();
+
+  body.appendChild(canvas);
+}(window, document));
