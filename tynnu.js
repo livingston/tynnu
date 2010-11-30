b042459ba051c84a7a71e733ad0f468d3914e619
Livingston Samuel <livingston.samuel@gmail.com>
2010-11-30 16:43:29 +0530

    Added draw feature - click and drag to draw

--- tynnu.js
+++ tynnu.js
@@ -8,7 +8,7 @@
 /*! tynnu.js
 
   @author - Livingston Samuel
-  @version - 0.1
+  @version - 0.2
 */
 
 (function (window, document) {
@@ -17,10 +17,28 @@
       gridW = body.clientWidth,
       gew = 10,
       canvas = document.createElement('canvas'),
+      canvas2 = document.createElement('canvas'),
       context = canvas.getContext('2d'),
+      context2 = canvas.getContext('2d'),
       n_x = parseInt(gridH/gew, 10) + 1,
       n_y = parseInt(gridW/gew, 10) + 1,
-      t;
+      handleDraw = function (e) {
+        e = e || event;
+        var x = roundTo(e.x, gew),
+            y = roundTo(e.y, gew);
+
+          context2.beginPath(x, y);
+          context2.fillStyle = 'rgba(6,100,195, 0.5)';
+          context2.rect(x,y, gew, gew);
+          context2.fill();
+          context2.save();
+      },
+      roundTo = function (n, x) {
+        var i = x/2,
+            j = n%x,
+            k = j>i? (x-j):(j*-1);
+        return (n + k)
+      }, t;
 
   canvas.width = gridW;
   canvas.height = gridH;
@@ -30,6 +48,13 @@
   canvas.style.zIndex = '1000000';
   context.lineWidth = 0.3;
   context.strokeStyle = 'rgba(0, 0, 0, 0.5)';
+
+  canvas2.width = gridW;
+  canvas2.height = gridH;
+  canvas2.style.position = 'absolute';
+  canvas2.style.top = '0';
+  canvas2.style.left = '0';
+  canvas2.style.zIndex = '1000001';
 
   context.beginPath(0, 0);
 
@@ -49,5 +74,13 @@
   context.fill();
   context.save();
 
+  canvas2.addEventListener('mousedown', function (e) {
+    canvas2.addEventListener('mousemove', handleDraw, false);
+  }, false);
+  canvas2.addEventListener('mouseup', function (e) {
+    canvas2.removeEventListener('mousemove', handleDraw, false);
+  }, false);
+
   body.appendChild(canvas);
+  body.appendChild(canvas2);
 }(window, document));
